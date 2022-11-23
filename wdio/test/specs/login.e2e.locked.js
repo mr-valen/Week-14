fe20832e88c1dc574e6824e210addcf64573287b
Valentín Michilena <87836739+mr-valen@users.noreply.github.com>
2022-11-22 21:46:06 -0300

    BaSP-W14: Added tests to login with locked user

---
+++ wdio/test/specs/login.e2e.locked.js
@@ -0,0 +1,14 @@
+import LoginPage from  '../pageobjects/login.page';
+import ProductsPage from '../pageobjects/products.page';
+
+describe('SwagLabs Login application', () => {
+    beforeAll('Navigate to url', () => {
+        browser.url("https://www.saucedemo.com/")
+    })
+    it('should not login and give feedback when user is locked', async () => {
+        await LoginPage.login('locked_out_user', 'secret_sauce');
+        await expect(browser).toHaveUrl("https://www.saucedemo.com/");
+        await expect(LoginPage.errorMsg).toBeDisplayedInViewport();
+        await expect(LoginPage.errorMsg).toHaveText("Epic sadface: Sorry, this user has been locked out.");
+    });
+});
